a8dbb1144066e208a69c592ce16a553cc50f9062
Rohit Parida <paridarohit38@gmail.com>
2024-08-10 16:51:01 +0530

    commit

---
+++ cn/operators.js
@@ -0,0 +1,17 @@
+// operators in JS
+// 1. Assignment Operator(=)\
+
+let a=10;
+let b=a;
+let c= a = 20;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// 2. Arithematic Operators(+,-,*,/,%,//)
+console.log(10+2);
+// 3.More Arithematic Operators(++,--,+,-,**)
+
+let i=10;
+i+=1;
+console.log(i);
---
+++ cn/string.js
@@ -0,0 +1,17 @@
+/* methods in string in JS */
+
+let str='JAVAscirpt is Wonderful';
+console.log(str.__proto__);
+
+console.log(str.charAt(1));
+// console.log(str.charAt(0));
+console.log(str.toLowerCase(str));
+let str1="JOHN";
+let str2="DOE";
+str1=str1.toLowerCase(str1);
+str2=str2.toLowerCase(str2);
+console.log(str1.toLowerCase(str1));
+console.log(str2.toLowerCase(str2));
+let answer;
+answer=str1.concat(str2);
+console.log(answer);
