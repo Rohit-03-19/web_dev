cf44887f7ae6d5e8a2408b24ca0e85f3f191dac8
Rohit-03-19 <paridarohit38@gmail.com>
2024-08-16 15:43:34 +0530

    initial commit

---
+++ cn/project/emailvalidation.js
@@ -0,0 +1,7 @@
+// we know some basic Email validation rules according to that we are going to make a program for that.
+
+const email=prompt("Enter the email");
+console.log(email);
+
+const emailLen=email.length;
+console.log(emailLen);
