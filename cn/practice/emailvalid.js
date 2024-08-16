5c4649717d13550571c4aca0359715cf21fffe30
Rohit-03-19 <paridarohit38@gmail.com>
2024-08-16 21:54:16 +0530

    commit

---
+++ cn/practice/emailvalid.js
@@ -0,0 +1,18 @@
+
+const email=prompt("Please Enter your email id");
+
+const emailLen= email.length;
+
+const dotindex= email.indexOf(".");
+
+const atindex = email.indexOf("@");
+
+const lastindex = emailLen- 1;
+
+if(emailLen<11 || 
+    lastindex-dotindex  < 2 ||
+    lastindex-dotindex > 3 ||
+    dotindex-atindex < 3
+){
+    console.log("Email is not valid");
+}
