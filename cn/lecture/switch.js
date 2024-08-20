17c6d9a601d5471273685892427e43324ccb3606
Rohit-03-19 <paridarohit38@gmail.com>
2024-08-20 18:05:06 +0530

    commit

---
+++ cn/lecture/switch.js
@@ -0,0 +1,23 @@
+/** Switch statemnet */
+
+// Stimulate a traffic light using switch
+
+let color = "red";
+
+switch (color) {
+  case "red":
+    console.log("STOP! the light is red.");
+    break;
+
+  case "yellow":
+    console.log("Caution! the light is yellow");
+    break;
+
+  case "green":
+    console.log("GO AHEAD!");
+    break;
+
+  default:
+    console.log("Invalid Color");
+    break;
+}
