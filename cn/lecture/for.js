96e15bd4abead17027919afa408d2277be7c2869
Rohit-03-19 <paridarohit38@gmail.com>
2024-08-20 18:55:52 +0530

    coding with passion

--- cn/lecture/for.js
+++ cn/lecture/for.js
@@ -1,6 +1,29 @@
 //   Iteration - For Loops
 //   print a message 10 times
 
-for (let i = 1; i <= 20; i++) {
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+
+for (let i = 0; ; i++) {
   console.log(i);
+  i < 5;
+}
+
+for (let i = 0; i < 5; ) {
+  console.log(i);
+}
+
+let j = 0;
+for (; j < 5; j++) {
+  console.log(j);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; ; i++) {
+  console.log(i);
+  i < 5;
 }
