db3e8ecbcb29606068b03b0ac6e429b64a484458
Rohit-03-19 <paridarohit38@gmail.com>
2024-08-20 18:39:34 +0530

    commit

--- cn/lecture/for.js
+++ cn/lecture/for.js
@@ -1,6 +1,6 @@
 //   Iteration - For Loops
 //   print a message 10 times
 
-for (let i = 1; (i = 10); i++) {
-  console.log("Hello World");
+for (let i = 1; (i = 20); i++) {
+  console.log("i");
 }
