ecbb78eec6419bff2ff1e6d9855017a698142608
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-08 15:21:48 +0530

    commit

--- apna college/assignment ques/jspart2.js
+++ apna college/assignment ques/jspart2.js
@@ -1,10 +1,19 @@
 /** Create a numebr variable num with some value.
  * Now, print "good" if the number is divisble by 10 and print "bad" if t not.
  */
-let num;
-num = prompt("Enter a number");
-if (num % 10 == 0) {
-  console.log("Goood");
-} else {
-  console.log("baad");
-}
+// let num;
+// num = prompt("Enter a number");
+// if (num % 10 == 0) {
+//   console.log("Goood");
+// } else {
+//   console.log("baad");
+// }
+
+/** Take the user's name & age as input using prompts.
+ * Then return back the following statement to the user as an alert(by subsituting thier name & age):
+ */
+
+let name, age;
+name = prompt("Enter the name");
+age = prompt("Enter the age");
+console.log(`${name} is ${age} years old.`);
