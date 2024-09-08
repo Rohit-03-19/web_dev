842dcbce7da35d841338257674e67c2f8d1d3d5a
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-08 12:19:59 +0530

    commit

---
+++ apna college/assignment ques/jspart2.js
@@ -0,0 +1,10 @@
+/** Create a numebr variable num with some value.
+ * Now, print "good" if the number is divisble by 10 and print "bad" if t not.
+ */
+let num;
+num = prompt("Enter a number");
+if (num % 10 == 0) {
+  console.log("Goood");
+} else {
+  console.log("baad");
+}
