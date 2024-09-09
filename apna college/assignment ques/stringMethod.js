53b797cf5f6d790838e6ae0d48ed51985219e5f8
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-09 18:05:33 +0530

    commit

--- apna college/assignment ques/stringMethod.js
+++ apna college/assignment ques/stringMethod.js
@@ -6,7 +6,15 @@
 
 let msg = "    Hello ";
 let trimMsg = msg.trim();
+let str = "i love my mom very much.";
 /**this trim method removes excess space from the starting and ending of the string.
  * Strings are immutable in JS. so if use any kind of method/ operations on string they get actually stored in a new different string.
  */
 console.log(msg, "", trimMsg);
+console.log(trimMsg.toLowerCase());
+console.log(trimMsg.toUpperCase());
+console.log(str.indexOf("my"));
+
+/** string slicing
+ * str.slice(starting index , ending index);
+ */
