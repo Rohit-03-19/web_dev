128c58ed8516f5b2475232d4cee4c79218f72f8f
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-10 17:29:03 +0530

    commit

---
+++ apna college/lectures/array.js
@@ -0,0 +1,20 @@
+/** Array (Data Structure).
+ * It is a linear collection of things.
+ */
+
+let student1 = "aman";
+let student2 = "ROHIT";
+let student3 = "jaron";
+
+/**instead of doing this we can make directly a array and store the values. */
+
+let students = ["aman", "rohit", "jaron"];
+
+// mixed array can also be made i.e. array having values of different data types.
+
+let months = ["january", "july", "march", "august"];
+months.shift();
+months.shift();
+months.unshift("june");
+months.unshift("july");
+console.log(months);
