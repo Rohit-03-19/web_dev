00643ebfff31fe7e03643f6f1cd80b1d91023d6a
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-12 17:50:06 +0530

    go on champ you can do more

--- apna college/lectures/array2.js
+++ apna college/lectures/array2.js
@@ -21,3 +21,16 @@ console.log(colors.splice(0, 1, "black", "grey"));
 console.log(colors.sort());
 
 let marks = [99, 89, 67, 42, 100];
+
+// array refrences
+// this basically tells us the conscept of memory address where the values are actually stored or the memory address reserved for the identifier given to that array.
+// in simple words if we compare two array let suppose arr1 and arr2 having value [1,2,3,4].
+// will give false because the values are not compared the memory address is compared and the address reserved is different for each array.
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4];
+if (arr1 == arr2) {
+  console.log("true");
+} else {
+  console.log("false");
+}
