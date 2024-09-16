f989174a19146f976667b313bf5b67e00157a9eb
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-16 13:41:22 +0530

    commit

---
+++ apna college/practice/lines.js
@@ -0,0 +1,6 @@
+function lineswithemotion() {
+  console.log(
+    "It's not the GOODBYE that hurts, but the flashbacks that follow."
+  );
+  console.log("");
+}
---
+++ apna college/practice/love.js
@@ -0,0 +1,7 @@
+console.log(
+  "hey dear please come back na missing you so much. Laado please comeback. Iassure you that i will not create a mess anymore."
+);
+console.log(
+  "Yrr sachme missing that prettiest moment you gave me, holding my hand while sitting  beside me.I just can't get over it."
+);
+console.log("You left me but your soul couldn't.");
