0fbe55e7e1f41f71f5316ab415d26b9cb4c605af
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-16 11:59:03 +0530

    commit

---
+++ apna college/assignment ques/jspart5.js
@@ -0,0 +1,13 @@
+/**Create a program that generates a random number representing  dice roll. */
+
+let diceOutcome = Math.floor(Math.random() * 6) + 1;
+console.log(`the number on dice is ${diceOutcome}`);
+
+/**Q2. Create an object representing a car that stores the following properties for the car, car: name,model,color. */
+
+let car = {
+  bmw: ["X5", "M-sport", "blue"],
+  audi: ["A8L", "Quatro", "black"],
+};
+
+/**Q3. Create an object Person with their name, age and city.*/
