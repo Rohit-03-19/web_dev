a8f51149ad1056bd9c508eec5a6511f44d726475
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-22 20:34:34 +0530

    commit

---
+++ cn/practice/noofwords.js
@@ -0,0 +1,31 @@
+// Problem statement
+// You are assigned the task of developing a program to count the number of words in a given string. Count and return the number of words present in the string.
+
+// Note
+// An extra space has been inserted after the last word of the string
+// Expected Input
+// "I am a Ninja Coder "
+// Expected Output
+// 5
+// Test Cases
+// Count the total number of words in the given string
+// Return the count of words
+
+function countWords(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 1; j < str.length; j++) {
+      if (str.charAt(i) === str.charAt(j)) {
+        continue;
+      } else if (str.charAt(i) === " " || str.charAt(j) == " ") {
+        continue;
+      } else {
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+
+string = "i am a ninja coder ";
+console.log(countWords(string));
