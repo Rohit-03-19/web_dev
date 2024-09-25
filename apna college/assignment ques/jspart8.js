cfb4eccc6e86baf3803854d0aa195695d37b2b34
Rohit-03-19 <paridarohit38@gmail.com>
2024-09-25 11:39:11 +0530

    commit

--- apna college/assignment ques/jspart8.js
+++ apna college/assignment ques/jspart8.js
@@ -18,7 +18,20 @@ let newarray = arr.map((ele) => (ele += 5));
 /**Q3. Create a new array whose elemnts are in uppercase of words present in the original array. */
 
 let names = ["tony", "bruce", "peter", "steve"];
+let Names = names.map((string) => string.toUpperCase());
 
 /**Q4. Write a funciton called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of the additional arguments doubled. */
+
+// const doubleAndReturnArgs = (arr, ...args) => [
+//   ...arr,
+//   ...args.map((ele) => ele * 2),
+// ];
+
+const doubleAndReturnArgs = (arr, ...args) => [
+  ...arr,
+  ...args.map((v) => v * 2),
+];
+doubleAndReturnArgs([1, 2, 3], 4, 4); //[1,2,3,8,8]
+doubleAndReturnArgs([2], 10, 4); //[2,20,8]
 
 /**Q5. Write a function called mergeObjects that accepts two objects and returns a new object which containes all the keys and values of the first object and second object. */
