18e6d769a8dd5e02b0f45efb6c363f2bfc11a883
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-01 19:48:22 +0530

    commit

--- apna college/lectures/dom.js
+++ apna college/lectures/dom.js
@@ -1,5 +1,23 @@
-let smallImages = document.getElementsByClassName("oldImg");
-for (let i = 0; i < smallImages.length; i++) {
-  smallImages[i].src = "../images/spiderman_img.png";
-  console.log(`values of image no. ${i} is changed.`);
-}
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < smallImages.length; i++) {
+//   smallImages[i].src = "../images/spiderman_img.png";
+//   console.log(`values of image no. ${i} is changed.`);
+// }
+
+/** Using properties and Methods
+ * first we will se text based.
+ * INNERTEXT :- shows te visible text contained in a node
+ *
+ * TEXTCONTENT :- shows all the full text.
+ *
+ * INNERHTML :- shows the full markup.
+ */
+
+// let heading = document.querySelector("h1");
+// console.log(heading);
+// heading.innerHTML = "<u>Spider man </u>";
+
+/** Manipulating Attributes
+ * abj.getAttribute( attr )
+ * abj.setAttribute( attr, val)
+ */
