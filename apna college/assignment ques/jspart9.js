ac86cce11030d396f6b713f0a6d73eea2075aec2
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-18 11:39:21 +0530

    commit

--- apna college/assignment ques/jspart9.js
+++ apna college/assignment ques/jspart9.js
@@ -22,4 +22,13 @@ but.style.color = "white";
 
 /** Q4. Create an h1 elemet on the page and set its text tp "DOM Practice " underlined. Change its color to purple. */
 
-let h1 = dcument.createElement("h1");
+let h1 = document.createElement("h1");
+h1.innerText = "DOM Practice";
+document.querySelector("body").append(h1);
+h1.classList.add("h1");
+
+/** Q5. Create a p tag on the page and st its text to "Apna College Delta Practice", where Delta is bold. */
+
+let para = document.createElement("p");
+para.innerHTML = "Apna College <b>Delta</b> Practice.";
+document.querySelector("body").append(para);
