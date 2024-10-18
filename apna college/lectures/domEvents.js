742e5f232c173fa6bb3e971b08264b26d713217c
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-18 12:39:57 +0530

    commit

---
+++ apna college/lectures/domEvents.js
@@ -0,0 +1,18 @@
+let btns = document.querySelectorAll("button");
+// console.dir(btn);
+
+// btn.onclick = function () {
+//   console.log("button was clicked");
+// };
+// for (btn of btns) {
+//   btn.onclick = sayHello;
+//   btn.onmouseenter = function () {
+//     console.log("youe have enetered the button.");
+//   };
+// }
+
+// function sayHello() {
+//   alert("Hello!");
+// }
+
+// // btn.onclick = sayHello;
