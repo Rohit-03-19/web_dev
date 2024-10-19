ac43e3eaa8b1178df190322ee1207b88d080fe69
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-19 23:13:50 +0530

    commit

--- apna college/lectures/domEvents.js
+++ apna college/lectures/domEvents.js
@@ -17,14 +17,41 @@
 // function sayName() {
 //   alert("Apna College!");
 // }
-let btn = document.querySelector("button");
+// let btn = document.querySelector("button");
 
-btn.addEventListener("click", function () {
-  console.log(this.innerText);
-  console.dir(this);
-  this.style.backgroundColor = `rgb(${Math.floor(
-    Math.random() * 255
-  )} , ${Math.floor(Math.random() * 255)} , ${Math.floor(
-    Math.random() * 255
-  )})`;
+// btn.addEventListener("click", function () {
+//   console.log(this.innerText);
+//   console.dir(this);
+//   this.style.backgroundColor = `rgb(${Math.floor(
+//     Math.random() * 255
+//   )} , ${Math.floor(Math.random() * 255)} , ${Math.floor(
+//     Math.random() * 255
+//   )})`;
+// });
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function (event) {
+//   console.log(event);
+//   console.log("button clicked");
+// });
+
+// let inp = document.querySelector("input");
+
+// inp.addEventListener("keyup", function (event) {
+//   console.log("key = ", event.key);
+//   console.log("code = ", event.code);
+//   console.log("key was released");
+// });
+
+// // inp.addEventListener("keydown", function () {
+// //   console.log("key was pressed");
+// // });
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function () {
+  alert("Form Submitited");
+  event.preventDefault();
+  console.log("Form Submitited");
 });
