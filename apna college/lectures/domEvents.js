12f9214ee23af825ea194f992688c4d0c1f84adc
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-19 12:36:56 +0530

    commit

--- apna college/lectures/domEvents.js
+++ apna college/lectures/domEvents.js
@@ -18,23 +18,35 @@
 //   alert("Apna College!");
 // }
 
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function () {
+//   // console.log("Generate random color");
+//   let h3 = document.querySelector("h3");
+//   let randomColor = getRandomColor();
+//   h3.innerText = randomColor;
+//   let div = document.querySelector("div");
+//   div.style.backgroundColor = randomColor;
+//   console.log("Color Updated");
+// });
+
+// function getRandomColor() {
+//   let red = Math.floor(Math.random() * 255);
+//   let green = Math.floor(Math.random() * 255);
+//   let blue = Math.floor(Math.random() * 255);
+
+//   let color = `rgb(${red},${green},${blue})`;
+//   return color;
+// }
+
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", function () {
-  // console.log("Generate random color");
-  let h3 = document.querySelector("h3");
-  let randomColor = getRandomColor();
-  h3.innerText = randomColor;
-  let div = document.querySelector("div");
-  div.style.backgroundColor = randomColor;
-  console.log("Color Updated");
+  console.log(this.innerText);
+  console.dir(this);
+  this.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )} , ${Math.floor(Math.random() * 255)} , ${Math.floor(
+    Math.random() * 255
+  )})`;
 });
-
-function getRandomColor() {
-  let red = Math.floor(Math.random() * 255);
-  let green = Math.floor(Math.random() * 255);
-  let blue = Math.floor(Math.random() * 255);
-
-  let color = `rgb(${red},${green},${blue})`;
-  return color;
-}
