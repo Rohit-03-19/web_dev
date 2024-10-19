69fe1f700132075f18ac7fc62fe72e150efb7931
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-19 11:49:02 +0530

    commit

--- apna college/lectures/domEvents.js
+++ apna college/lectures/domEvents.js
@@ -1,19 +1,40 @@
-let btns = document.querySelectorAll("button");
-// console.dir(btn);
+// let btns = document.querySelectorAll("button");
+// // console.dir(btn);
 
-// btn.onclick = function () {
-//   console.log("button was clicked");
-// };
-for (btn of btns) {
-  btn.addEventListener("click", sayHello);
-  btn.addEventListener("click", sayName);
-}
+// // btn.onclick = function () {
+// //   console.log("button was clicked");
+// // };
+// for (btn of btns) {
+//   btn.addEventListener("click", sayHello);
+//   btn.addEventListener("click", sayName);
+// }
 
-// // btn.onclick = sayHello;
+// // // btn.onclick = sayHello;
 
-function sayHello() {
-  alert("Hello!");
-}
-function sayName() {
-  alert("Apna College!");
+// function sayHello() {
+//   alert("Hello!");
+// }
+// function sayName() {
+//   alert("Apna College!");
+// }
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  // console.log("Generate random color");
+  let h3 = document.querySelector("h3");
+  let randomColor = getRandomColor();
+  h3.innerText = randomColor;
+  let div = document.querySelector("div");
+  div.style.backgroundColor = randomColor;
+  console.log("Color Updated");
+});
+
+function getRandomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  let color = `rgb(${red},${green},${blue})`;
+  return color;
 }
