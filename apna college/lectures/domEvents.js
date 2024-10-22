62b90b99f8f22dc19666f457f786cb078a56d783
Rohit-03-19 <paridarohit38@gmail.com>
2024-10-22 09:02:48 +0530

    commit

--- apna college/lectures/domEvents.js
+++ apna college/lectures/domEvents.js
@@ -50,8 +50,11 @@
 
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function () {
-  alert("Form Submitited");
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("Form Submitited");
+  let user = document.querySelector("#user");
+  let pass = document.querySelector("#pass");
+
+  console.log(user.value);
+  console.log(pass.value);
 });
