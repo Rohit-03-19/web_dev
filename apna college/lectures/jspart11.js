2abd2c089793bea3b10051d4bb6593c428def4a2
Rohit-03-19 <paridarohit38@gmail.com>
2024-12-05 22:26:06 +0530

    commit

--- apna college/lectures/jspart11.js
+++ apna college/lectures/jspart11.js
@@ -51,7 +51,7 @@ function changeColor(color, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let num = Math.floor(Math.random() * 5) + 1;
-      if (num > 2) {
+      if (num > 3) {
         reject("promise rejected");
       }
       h1.style.color = color;
@@ -62,10 +62,17 @@ function changeColor(color, delay) {
 }
 
 async function demo() {
-  await changeColor("red", 1000);
-  await changeColor("orange", 1000);
-  await changeColor("green", 1000);
-  await changeColor("blue", 1000);
+  try {
+    await changeColor("red", 1000);
+    await changeColor("orange", 1000);
+    await changeColor("green", 1000);
+    await changeColor("blue", 1000);
+  } catch (err) {
+    console.log("error caught");
+    console.log(err);
+  }
+  let a = 5;
+  console.log(a);
 }
 
 // let requestPromise = changeColor("red", 1000);
